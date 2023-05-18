857d17723ae927984481cd740583c643fbe6ad32
Thomas Peacock <tom.peacock@cyberthy.com>
2023-05-18 15:21:07 +0100

    init commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'path';
+const app = express();
+const port = 3001; // default port to listen
+
+// Configure Express to use EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// define a route handler for the default home page
+app.get('/', (req, res) => {
+  // render the index template
+  res.render('index');
+});
+
+// api routes
+// tasks crud
+app.get('/tasks', (req, res) => {
+  res.send('tasks');
+});
+
+// start the express server
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
